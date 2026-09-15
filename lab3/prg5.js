cb0109f8528188d76381fe5dcdffa61458f4cb74
Akash <avivishwakarma7yt@gmail.com>
2026-09-15 14:13:09 +0530

    Remove unused HTML files, JavaScript files, and package files from the project to streamline the codebase and eliminate unnecessary clutter.

---
+++ lab3/prg5.js
@@ -0,0 +1,28 @@
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+    // req method ->   GET, POST, PUT, DELETE, PATCH    
+    console.log("Method:", req.method);
+    console.log("URL:", req.url);
+    
+    // Browser can send only GET request to the server
+    // POST/PUT/PATCH/DELETE -> can be checked by api tester
+    // API Tester: - postman, echo api, thunder client
+
+    if (req.url === '/') {
+        res.setHeader("content-type", "text/html");
+        res.end("<h1>Hello Server</h1>");
+    } else if (req.url === '/products') {
+        const products = [
+            { id: 1, name: 'bottle' },
+            { id: 2, name: 'laptop' },
+
+        ]
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify(products));
+    } else {
+        res.end();
+    }
+});
+
+server.listen(5001, () => console.log("prog5 is running"));
